268905bb85bfcd86fb028b5a52ff77664cee7a72
ArnoDevWeb59 <arnodevweb59@gmail.com>
2024-04-19 16:11:25 +0200

    try to use a common table view but not supported in BQ

--- Materialized_View_UnionAllTables.sql
+++ Materialized_View_UnionAllTables.sql
@@ -1,12 +1,14 @@
-CREATE MATERIALIZED VIEW bge_2024.Leads_per_period.Mat_view_AggAlllists AS
-MERGE INTO `bge-2024.Leads_per_period.initial_table_to_29022024` AS target
-USING (
+WITH tvf_result AS (
   SELECT * FROM `bge-2024.Leads_per_period.LeadsAllListAggPartnersIds`
-) AS source
-ON target.Idpartner = source.Idpartner 
-   AND target.Entrepreneur = source.Entrepreneur 
-   AND target.date_without_time = source.date_without_time
-WHEN MATCHED THEN
-  UPDATE SET target.* = source.*
-WHEN NOT MATCHED THEN
-  INSERT ROW;
+)
+
+CREATE MATERIALIZED VIEW bge-2024.Leads_per_period.Mat_view_AggAlllists AS
+
+SELECT *
+FROM tvf_result;
+
+
+-- Insert new rows
+INSERT INTO `bge-2024.Leads_per_period.initial_table_to_29022024`
+SELECT *
+FROM `bge-2024.Leads_per_period.Mat_view_AggAlllists`
